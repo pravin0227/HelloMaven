4f2a24a6f48b5fddd1179a53ee892f1e4bb200c3
pravin0227 <Pravinmore0227@gmail.com>
2022-11-12 01:10:56 +0530

    initial commit

---
+++ mavenJenkin/src/main/java/com/maven/MavenJenkinApplication.java
@@ -0,0 +1,13 @@
+package com.maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenJenkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenJenkinApplication.class, args);
+	}
+
+}
